35538a71c88bc1ef6de809778f65745a44362082
Aleksandre Khokhiashvili <alkhok18@freeuni.edu.ge>
2022-03-24 18:43:07 +0400

    feat: Lexer works

---
+++ Tiger.g4
@@ -0,0 +1,65 @@
+grammar Tiger;
+
+
+ARRAY: 'array';
+BEGIN: 'begin';
+BREAK: 'break';
+DO: 'do';
+ELSE: 'else';
+END: 'end';
+ENDDO: 'enddo';
+ENDIF: 'endif';
+FLOAT: 'float';
+FOR: 'for';
+FUNCTION: 'function';
+IF: 'if';
+INT: 'int';
+LET: 'let';
+OF: 'of';
+PROGRAM: 'program';
+RETURN: 'return';
+STATIC: 'static';
+THEN: 'then';
+TO: 'to';
+TYPE: 'type';
+VAR: 'var';
+WHILE: 'while';
+
+COMMA: ',';
+DOT: '.';
+COLON: ':';
+SEMICOLON: ';';
+OPENPAREN: '(';
+CLOSEPAREN: ')';
+OPENBRACK: '[';
+CLOSEBRACK: ']';
+OPENCURLY: '{';
+CLOSECURLY: '}';
+
+PLUS: '+';
+MINUS: '-';
+MULT: '*';
+DIV: '/';
+POW: '**';
+EQUAL: '==';
+NEQUAL: '!=';
+LESS: '<';
+GREAT: '>';
+LESSEQ: '<=';
+GREATEQ: '>=';
+AND: '&';
+OR: '|';
+
+ASSIGN: ':=';
+TASSIGN: '=';
+
+COMMENT: '/*' .*? '*/' -> skip;
+ID: [a-zA-Z][a-zA-Z0-9_]*;
+INTLIT: ([1-9]DIGIT*) | '0';
+fragment DIGIT: [0-9];
+FLOATLIT: INTLIT '.' DIGIT*;
+
+
+WHITESPACE : [ \r\t\n]+ -> skip ;
+
+
--- src/com/tiger/Main.java
+++ src/com/tiger/Main.java
@@ -1,8 +1,45 @@
 package com.tiger;
 
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        if(!args[0].equals("-i") || args.length < 2) {
+            System.out.println("Usage: <program> -i filename");
+            return;
+        }
+        String filename = args[1];
+        String outfilename = args[2];
+
+        System.out.print("Parsing: " + filename);
+        CharStream charStream = CharStreams.fromPath(Path.of(filename));
+        TigerLexer tigerLexer = new TigerLexer(charStream);
+//        CommonTokenStream commonTokenStream = new CommonTokenStream(tigerLexer);
+//        System.out.print(commonTokenStream);
+
+        Vocabulary vocabulary = tigerLexer.getVocabulary();
+        List<? extends Token> allTokens = tigerLexer.getAllTokens();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outfilename));
+
+        for (Token token : allTokens) {
+
+            String symbolicName = vocabulary.getSymbolicName(token.getType());
+            String text = token.getText();
+            writer.write("<" + symbolicName + ", " + "\"" + text + "\"" +">\n");
+            System.out.println("<" + symbolicName + ", " + "\"" + text + "\"" +">");
+        }
+        writer.close();
+
+
+
     }
 }
