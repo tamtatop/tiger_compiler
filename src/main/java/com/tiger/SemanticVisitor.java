97088aceaa1fd20c380e51a903761135fb3aa5a3
Aleksandre Khokhiashvili <alkhok18@freeuni.edu.ge>
2022-04-22 13:48:25 +0400

    some experiments with visitor

--- src/main/java/com/tiger/SemanticVisitor.java
+++ src/main/java/com/tiger/SemanticVisitor.java
@@ -3,10 +3,34 @@ package com.tiger;
 import com.tiger.antlr.TigerBaseVisitor;
 import com.tiger.antlr.TigerParser;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 
-class SemanticVisitor extends TigerBaseVisitor<Integer> {
+
+class SemanticVisitor extends TigerBaseVisitor<Void> {
+
+    HashMap<String, ArrayList<Void>> symbolTable;
+
+
     @Override
-    public Integer visitTiger_program(TigerParser.Tiger_programContext ctx) {
-        return super.visitTiger_program(ctx);
+    public Void visitTiger_program(TigerParser.Tiger_programContext ctx) {
+        System.out.println("visiting tiger_program");
+        System.out.printf("program name: %s!%n", ctx.ID().getText());
+        visitRootDeclaration_segment(ctx.declaration_segment());
+        visit(ctx.funct_list());
+
+        return null;
     }
+
+    @Override
+    public Void visitDeclaration_segment(TigerParser.Declaration_segmentContext ctx) {
+        System.out.println("visiting declarationss");
+        return visitChildren(ctx);
+    }
+
+    public void visitRootDeclaration_segment(TigerParser.Declaration_segmentContext ctx) {
+        System.out.println("visiting root declarationss");
+        visitChildren(ctx);
+    }
+
 }
