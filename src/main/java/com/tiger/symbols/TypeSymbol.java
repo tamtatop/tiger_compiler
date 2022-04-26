aa8f6f996027298f07403c0dc57fd748af6834c6
Aleksandre Khokhiashvili <alkhok18@freeuni.edu.ge>
2022-04-26 16:29:37 +0400

    feat: implement type symbol

---
+++ src/main/java/com/tiger/symbols/TypeSymbol.java
@@ -0,0 +1,28 @@
+package com.tiger.symbols;
+
+import com.tiger.types.Type;
+
+public class TypeSymbol implements Symbol {
+    String name;
+    Type type;
+
+    public TypeSymbol(String name, Type type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public SymbolKind getSymbolKind() {
+        return SymbolKind.TYPE;
+    }
+
+    @Override
+    public String format() {
+        return String.format("%s, type, %s", name, type.format());
+    }
+}
