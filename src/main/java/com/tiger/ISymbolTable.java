66bfc93a52c23f2e869959c29e340b16c4398f49
Aleksandre Khokhiashvili <alkhok18@freeuni.edu.ge>
2022-04-27 16:25:40 +0400

    fix: add missing exception

--- src/main/java/com/tiger/ISymbolTable.java
+++ src/main/java/com/tiger/ISymbolTable.java
@@ -2,8 +2,19 @@ package com.tiger;
 
 import com.tiger.symbols.Symbol;
 
+import javax.management.openmbean.KeyAlreadyExistsException;
+
+
+class SymbolTableDuplicateKeyException extends KeyAlreadyExistsException {
+    public SymbolTableDuplicateKeyException(String msg) {
+        super(msg);
+    }
+}
+
 interface ISymbolTable {
-    void insertSymbol(Symbol symbol);
+    void insertSymbol(Symbol symbol) throws SymbolTableDuplicateKeyException;
+
+    String curScopeName();
 
     Symbol getSymbol(String name);
 
