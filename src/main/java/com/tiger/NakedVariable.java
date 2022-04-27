745a9c6decbb808f1853970587710969a1018531
Aleksandre Khokhiashvili <alkhok18@freeuni.edu.ge>
2022-04-27 18:51:17 +0400

    feat: Introduce NakedVariable

---
+++ src/main/java/com/tiger/NakedVariable.java
@@ -0,0 +1,9 @@
+package com.tiger;
+
+import com.tiger.types.TypeStructure;
+
+public class NakedVariable {
+    public String name;
+    public String scopeName;
+    public TypeStructure typeStructure;
+}
