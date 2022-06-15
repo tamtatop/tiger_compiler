ac73339726fcf409bf6e80474c7d388184218b10
tamtatop <ttopu18@freeuni.edu.ge>
2022-06-15 14:48:40 +0400

    feat: started LoadedVariable implementation

---
+++ src/main/java/com/tiger/backend/LoadedVariable.java
@@ -0,0 +1,25 @@
+package com.tiger.backend;
+
+import com.tiger.BackendVariable;
+
+public class LoadedVariable {
+    String loadedRegister;
+    BackendVariable backing;
+
+    public LoadedVariable(BackendVariable backing, TemporaryRegisterAllocator tempAllocator) {
+        this.backing = backing;
+    }
+
+    String loadAssembly() {
+        // assembly to load variable value into liftedRegister
+        return "";
+    }
+
+    String getReg() {
+        return loadedRegister;
+    }
+
+    String flushAssembly() {
+        return "";
+    }
+}
